6df4fe8956ebe14e40f0eb6451330ddf5a628d7c
JhsK <qwe6293@nate.com>
2021-02-25 15:36:07 +0900

    삭제 후 다시 실습 필요

---
+++ learn-redux-middleware/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SampleContainer from "./containers/SampleCounter";
+
+const App = () => {
+  return (
+    <div>
+      <SampleContainer />
+    </div>
+  );
+};
+
+export default App;
