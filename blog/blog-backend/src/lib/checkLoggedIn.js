cef5309660ecf8adc504ceed957b1fe630d99fa3
JhsK <qwe6293@nate.com>
2021-03-12 11:12:18 +0900

    ctx.state error

---
+++ blog/blog-backend/src/lib/checkLoggedIn.js
@@ -0,0 +1,10 @@
+const checkLoggedIn = (ctx, next) => {
+  console.log('ctx.state.user : ' + ctx.state.user);
+  if (!ctx.state.user) {
+    ctx.status = 401;
+    return;
+  }
+  return next();
+};
+
+export default checkLoggedIn;
